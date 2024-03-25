babbcda9ebed70bf5f45e896520770a6e7873236
mleal <mleal@indexar.cl>
2024-03-25 09:23:37 -0300

    git init

---
+++ src/test/java/com/matias/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.curso.springboot.jpa.springbootjparelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
