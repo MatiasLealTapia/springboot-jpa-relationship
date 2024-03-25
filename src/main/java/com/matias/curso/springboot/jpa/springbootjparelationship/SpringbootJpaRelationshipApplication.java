5144e1c6650f4e1d5d02e3019522eb0427784226
mleal <mleal@indexar.cl>
2024-03-25 15:34:31 -0300

    Asociaciones @ManyToOne y anotacion @JoinColumn

--- src/main/java/com/matias/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
+++ src/main/java/com/matias/curso/springboot/jpa/springbootjparelationship/SpringbootJpaRelationshipApplication.java
@@ -1,13 +1,61 @@
 package com.matias.curso.springboot.jpa.springbootjparelationship;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.matias.curso.springboot.jpa.springbootjparelationship.entities.Client;
+import com.matias.curso.springboot.jpa.springbootjparelationship.entities.Invoice;
+import com.matias.curso.springboot.jpa.springbootjparelationship.repositories.ClientRepository;
+import com.matias.curso.springboot.jpa.springbootjparelationship.repositories.InvoiceRepository;
 
 @SpringBootApplication
-public class SpringbootJpaRelationshipApplication {
+public class SpringbootJpaRelationshipApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		manyToOneFindByIdClient();
+	}
+
+	@Transactional
+	public void manyToOne() {
+
+		Client client = new Client("John", "Doe");
+		clientRepository.save(client);
+
+		Invoice invoice = new Invoice("Compras de oficina", 2000L);
+		invoice.setClient(client);
+		Invoice invoiceDB = invoiceRepository.save(invoice);
+		System.out.println(invoiceDB);
+	}
+
+	@Transactional
+	public void manyToOneFindByIdClient() {
+
+		Optional<Client> optionalClient = clientRepository.findById(1L);
+
+		if (optionalClient.isPresent()) {
+			Client client = optionalClient.orElseThrow();
+			Invoice invoice = new Invoice("Compras de oficina", 2000L);
+			invoice.setClient(client);
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+			System.out.println(invoiceDB);
+		}
+
 	}
 
 }
---
+++ src/main/java/com/matias/curso/springboot/jpa/springbootjparelationship/repositories/InvoiceRepository.java
@@ -0,0 +1,9 @@
+package com.matias.curso.springboot.jpa.springbootjparelationship.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.matias.curso.springboot.jpa.springbootjparelationship.entities.Invoice;
+
+public interface InvoiceRepository extends CrudRepository<Invoice, Long> {
+
+}
